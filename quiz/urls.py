bc077c96024d7680359938a6ef58701ad8bbc729
Andrew Sheerin <94998544+andrewsheerin@users.noreply.github.com>
2025-12-11 10:03:53 -0500

    Delete quiz directory

--- quiz/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'quiz'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('api/countries/', views.get_countries, name='get_countries'),
-]
